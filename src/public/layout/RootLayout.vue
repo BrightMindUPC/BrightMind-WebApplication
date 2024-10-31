141dd4e8b35849f4b397eb4bc9687ad91bd53cbf
SgtGarro <marcelogarro137@gmail.com>
2024-10-30 20:57:12 -0500

    refactor: change base text color

--- src/public/layout/RootLayout.vue
+++ src/public/layout/RootLayout.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="flex w-full h-screen">
     <div class="w-60 shrink-0 bg-[#272727] text-white p-6">the aside menu</div>
-    <div class="w-full p-6">
+    <div class="w-full h-full p-6 bg-white text-gray-700">
       <router-view></router-view>
     </div>
   </main>
